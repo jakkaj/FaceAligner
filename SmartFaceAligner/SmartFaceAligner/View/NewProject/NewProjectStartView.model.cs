06de26b53f86099d3c2279389ad2f975a87e3368
Jordan Knight <jorkni@microsoft.com>
2017-02-05 07:56:03 +1100

    Starting the wizard

---
+++ SmartFaceAligner/SmartFaceAligner/View/NewProject/NewProjectStartView.model.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using XCoreLite.View;
+
+namespace SmartFaceAligner.View.NewProject
+{
+    public class NewProjectStartViewModel : ViewModel
+    {
+        private string _projectName;
+
+        public NewProjectStartViewModel()
+        {
+            
+        }
+
+        public string ProjectName
+        {
+            get { return _projectName; }
+            set
+            {
+                _projectName = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+}
