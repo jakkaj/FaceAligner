81f33cff34f685d96c2f83f714fed35695e7b16f
Jordan Knight <jakkaj@outlook.com>
2017-02-23 14:05:04 +1100

    Fix parapllel reference.

--- SmartFaceAligner/IntegrationTests/Tests/ParallelTests.cs
+++ SmartFaceAligner/IntegrationTests/Tests/ParallelTests.cs
@@ -5,7 +5,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using SmartFaceAligner.Util;
+using SmartFaceAligner.Processor;
+
 
 namespace IntegrationTests.Tests
 {
